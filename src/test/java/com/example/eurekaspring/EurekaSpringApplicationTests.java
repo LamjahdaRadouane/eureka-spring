1f02dfdd0ccdab8913d3b858f43961b47b587664
LAMJAHDA Radouane <RadouaneLam99@gmail.com>
2022-11-18 14:40:13 +0100

    Initial commit

---
+++ src/test/java/com/example/eurekaspring/EurekaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
